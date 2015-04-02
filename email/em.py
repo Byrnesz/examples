0687a8e7ae83ab397e68ad76af25adc0d79dab21
gwulfs <you@example.com>
2015-04-02 18:03:20 -0400

    rename

---
+++ email/em.py
@@ -0,0 +1,48 @@
+from indicoio import sentiment
+from email_reply_parser import EmailReplyParser
+
+
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+def email_to_words(raw_review):
+    review_text = BeautifulSoup(raw_review).get_text()
+    letters_only = re.sub("[^a-zA-Z]", " ", review_text)
+    words = letters_only.lower().split()
+    stops = set(stopwords.words("english"))
+    useful_words = [w for w in words if not w in stops]
+    return useful_words
+
+import pdb
+def sentiment_words_sliding(messages, window=10000, shift=200):
+    allwords = []
+    data = {}
+    for m in messages:
+        # pdb.set_trace()
+        # if "\\Sent" not in m.get('folders', tuple()):
+        #     continue
+        # if not m.get('body') or not m['body'].get('content'):
+        #     continue
+        allwords.extend(email_to_words(EmailReplyParser.parse_reply(m['body']['content'])))
+    current_window = 0
+    next_window = window
+    print len(allwords)
+    while True:
+        if len(allwords) < next_window:
+            print "sliding-sentiment reached end at lengths:%s" % len(allwords)
+            break
+        print "sliding-sentiment start:%s end:%s" % (current_window, next_window)
+        data[current_window] = ' '.join(allwords[current_window:next_window])
+        data[current_window] = sentiment(data[current_window])
+        current_window += shift
+        next_window += shift
+    return data
+
+
+import json
+f = open('email_4_1.json','r')
+rawemails = json.load(f)
+print len(rawemails)
+f.close()
+data = sentiment_words_sliding(rawemails)
+print data
