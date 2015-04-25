4715dd148397e59aac8e89e563dafecbd5bf7e6a
gwulfs <you@example.com>
2015-04-25 00:27:27 -0400

    extent vs append

--- email/em.py
+++ email/em.py
@@ -5,7 +5,6 @@ from email_reply_parser import EmailReplyParser
 import pickle
 import json
 
-
 def sentiment_sliding(messages, window=1000, shift=20):
     allwords = []
     data = {}
@@ -14,7 +13,11 @@ def sentiment_sliding(messages, window=1000, shift=20):
             continue
         if not m.get('body') or not m['body'].get('content'):
             continue
-        allwords.extend(EmailReplyParser.parse_reply(m['body']['content']))
+        allwords.append(EmailReplyParser.parse_reply(m['body']['content']))
+
+    allwords = " ".join(allwords)
+    allwords = allwords.encode('ascii','ignore')
+    allwords = allwords.split()
 
     current_window = 0
     next_window = window
@@ -26,12 +29,13 @@ def sentiment_sliding(messages, window=1000, shift=20):
         print "sliding-sentiment start:%s end:%s" % (current_window, next_window)
         data[current_window] = ' '.join(allwords[current_window:next_window])
         data[current_window] = indicoio.sentiment(data[current_window])
+        print data[current_window]
         current_window += shift
         next_window += shift
     return data
 
 
-f = open('email.json','r')
+f = open('emgw.json','r')
 rawemails = json.load(f)
 print 'number of emails', len(rawemails)
 f.close()
